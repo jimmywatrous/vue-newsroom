d872f05aba6330aff9fbf84e5a87a1e7acf3f69a
EC2 Default User <ec2-user@ip-172-31-95-249.ec2.internal>
2018-07-04 16:27:50 +0000

    Fixed card centering issue.

--- src/components/NewsRoom.vue
+++ src/components/NewsRoom.vue
@@ -1,23 +1,20 @@
 <template>
     <div class="container">
-        <h1>Newsroom</h1>
         <div class="row">
-            <div v-for="(item, index) in newsroom.articles" class="">
-                <div class="col offset-1">
-                    <div class="card mt-4" style="width: 20rem;">
-                        <div class="card-header">
-                            <span class="card-text text-muted">{{ newsroom.articles[index].source.name }}</span>
-                        </div>
-                        <img v-bind:src="newsroom.articles[index].urlToImage" alt="" class="card-img-top">
-                        <div class="card-body">
-                            <a v-bind:href="newsroom.articles[index].url" class="card-title">
-                                <h5>{{ newsroom.articles[index].title }}</h5>
-                            </a>
-                            <p class="card-text">{{ newsroom.articles[index].description }}</p>
-                            
-                        </div>
+            <div v-for="(item, index) in newsroom.articles" class="col">
+                <div class="card mt-4 mx-auto" style="width: 20rem;">
+                    <div class="card-header">
+                        <span class="card-text text-muted">{{ newsroom.articles[index].source.name }}</span>
                     </div>
-                </div>
+                    <img v-bind:src="newsroom.articles[index].urlToImage" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <a v-bind:href="newsroom.articles[index].url" class="card-title">
+                            <h5>{{ newsroom.articles[index].title }}</h5>
+                        </a>
+                        <p class="card-text">{{ newsroom.articles[index].description }}</p>
+                        
+                    </div>
+                    </div>
             </div>
         </div>
         <pre>{{newsroom}}</pre>
